d0ef814e80645f4f8093c698d85d324ba802cf90
Timothy Wu <t2wu@THW-air.local>
2013-08-08 18:22:49 +0800

    added test target

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Timothy Wu on 13/8/8.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,15 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Timothy Wu on 13/8/8.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "WowUtils.h"
+
+@interface WowUtilsTests : SenTestCase
+
+
+@end
